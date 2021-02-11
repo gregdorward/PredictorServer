ce76f6fed74e368a9bce6b0cb5d9d5c96d7ac460
Greg Dorward <greg.dorward@red-badger.com>
2021-02-11 10:15:39 +0000

    adding files

--- index.js
+++ index.js
@@ -31,19 +31,27 @@ const yesterday = `https://api.footystats.org/todays-matches?key=${process.env.R
 const today = `https://api.footystats.org/todays-matches?key=${process.env.REACT_APP_API_KEY}&date=${year}-${day}-${month}`;
 const tomorrow = `https://api.footystats.org/todays-matches?key=${process.env.REACT_APP_API_KEY}&date=${tomorrowYear}-${tomorrowDay}-${tomorrowMonth}`;
 
-app.use(cors({ origin: "https://gregdorward.github.io/Predictor" }));
+app.use(
+  cors({
+    origin: "https://gregdorward.github.io/Predictor",
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/static", express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 5000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT || 5000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
-
+  res.send("<h1>Hello World!</h1>");
+});
 
 app.get("/todaysFixtures", (req, res) => {
   fs.readFile("today.json", function (err, data) {
@@ -51,57 +59,203 @@ app.get("/todaysFixtures", (req, res) => {
     const fixtures = JSON.parse(data);
     res.send({ fixtures });
   });
-})
+});
 
 app.get("/tomorrowsFixtures", (req, res) => {
-    fs.readFile("tomorrow.json", function (err, data) {
-      if (err) return console.log(err);
-      const fixtures = JSON.parse(data);
-      res.send({ fixtures });
-    });
-  })
+  fs.readFile("tomorrow.json", function (err, data) {
+    if (err) return console.log(err);
+    const fixtures = JSON.parse(data);
+    res.send({ fixtures });
+  });
+});
 
+app.get("/yesterdaysFixtures", (req, res) => {
+  fs.readFile("yesterday.json", function (err, data) {
+    if (err) return console.log(err);
+    const fixtures = JSON.parse(data);
+    res.send({ fixtures });
+  });
+});
 
-app.get("yesterdaysFixtures", (req, res) => {
-    fs.readFile("yesterday.json", function (err, data) {
-        if(err) return console.log(err);
-        const fixtures = JSON.parse(data);
-        res.semd({fixtures})
-    })
-})
+app.post("/postPredictions5today", (req, res) => {
+  var now = new Date();
+  var hour = now.getHours();
+  if (hour <= 14) {
+    fs.writeFile(
+      `fixedPredictions5today.json`,
+      JSON.stringify(req.body),
+      function (err) {
+        if (err) return console.log(err);
+      }
+    );
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(400)
+  }
+});
+
+app.post("/postPredictions5tomorrowsFixtures", (req, res) => {
+  var now = new Date();
+  var hour = now.getHours();
+  if (hour <= 14) {
+    fs.writeFile(
+      `fixedPredictions5tomorrow.json`,
+      JSON.stringify(req.body),
+      function (err) {
+        if (err) return console.log(err);
+      }
+    );
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(400)
+  }
+});
+
+app.post("/postPredictions6todaysFixtures", (req, res) => {
+  var now = new Date();
+  var hour = now.getHours();
+  if (hour <= 14) {
+    fs.writeFile(
+      `fixedPredictions6today.json`,
+      JSON.stringify(req.body),
+      function (err) {
+        if (err) return console.log(err);
+      }
+    );
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(400)
+  }
+});
+
+app.post("/postPredictions6tomorrowsFixtures", (req, res) => {
+  var now = new Date();
+  var hour = now.getHours();
+  if (hour <= 14) {
+    fs.writeFile(
+      `fixedPredictions6tomorrow.json`,
+      JSON.stringify(req.body),
+      function (err) {
+        if (err) return console.log(err);
+      }
+    );
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(400)
+  }
+});
+
+app.post("/postPredictions10todaysFixtures", (req, res) => {
+  var now = new Date();
+  var hour = now.getHours();
+  if (hour <= 14) {
+    fs.writeFile(
+      `fixedPredictions10today.json`,
+      JSON.stringify(req.body),
+      function (err) {
+        if (err) return console.log(err);
+      }
+    );
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(400)
+  }
+});
+
+app.post("/postPredictions10tomorrowsFixtures", (req, res) => {
+  var now = new Date();
+  var hour = now.getHours();
+  if (hour <= 14) {
+    fs.writeFile(
+      `fixedPredictions10tomorrow.json`,
+      JSON.stringify(req.body),
+      function (err) {
+        if (err) return console.log(err);
+      }
+    );
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(400)
+  }
+});
+
+app.get("/yesterdaysPredictions5", (req, res) => {
+  fs.readFile("fixedPredictions5yesterday.json", function (err, data) {
+    if (err) return console.log(err);
+    const fixtures = JSON.parse(data);
+    res.send({ fixtures });
+  });
+});
 
 
 async function getFixtureList(day, string) {
-    await fetch(`https://safe-caverns-99679.herokuapp.com/${day}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Origin: "https://gregdorward.github.io",
-        Host: "safe-caverns-99679.herokuapp.com",
-      },
-    })
-      .then((res) => res.json())
-      .then((res) => fs.writeFile(`${string}.json`, JSON.stringify(res.data), function (err) {
-          if (err) return console.log(err);
-        }))
-      .catch(err => console.log(err))
-  }
-
+  await fetch(`https://safe-caverns-99679.herokuapp.com/${day}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Origin: "https://gregdorward.github.io",
+      Host: "safe-caverns-99679.herokuapp.com",
+    },
+  })
+    .then((res) => res.json())
+    .then((res) =>
+      fs.writeFile(`${string}.json`, JSON.stringify(res.data), function (err) {
+        console.log(`file ${string} written`)
+        if (err) return console.log(err);
+      })
+    )
+    .catch((err) => console.log(err));
+}
 
 const rule = new schedule.RecurrenceRule();
-rule.hour = [new schedule.Range(00, 12)]
+rule.hour = [new schedule.Range(00, 12)];
 rule.minute = 00;
 
-const job = schedule.scheduleJob(rule, async function(){
-    await getFixtureList(today, "today");
-    await getFixtureList(tomorrow, "tomorrow");
-    console.log("automatically fetched today's games");
-    console.log("automatically fetched tomorrow's games");
-  });
-
+const job = schedule.scheduleJob(rule, async function () {
+  await getFixtureList(today, "today");
+  await getFixtureList(tomorrow, "tomorrow");
+  console.log("automatically fetched today's games");
+  console.log("automatically fetched tomorrow's games");
+});
 
 const job2 = schedule.scheduleJob("59 23 * * *", async function () {
-    fs.rename('today.json', 'yesterday.json', (err) => {
-        if (err) throw err;
-        console.log('Rename complete!');
-      });
+  fs.rename("today.json", "yesterday.json", (err) => {
+    if (err) throw err;
+    console.log("Rename 1 complete!");
+  });
+
+  fs.rename("tomorrow.json", "today.json", (err) => {
+    if (err) throw err;
+    console.log("Rename 2 complete!");
+  });
+
+  fs.rename("fixedPredictions5today", "fixedPredictions5yesterday.json", (err) => {
+    if (err) throw err;
+    console.log("Rename 3 complete!");
+  });
+
+  fs.rename("fixedPredictions6today", "fixedPredictions6yesterday.json", (err) => {
+    if (err) throw err;
+    console.log("Rename 4 complete!");
+  });
+
+  fs.rename("fixedPredictions10today", "fixedPredictions10yesterday.json", (err) => {
+    if (err) throw err;
+    console.log("Rename 5 complete!");
+  });
+
+  fs.rename("fixedPredictions5tomorrow", "fixedPredictions5today.json", (err) => {
+    if (err) throw err;
+    console.log("Rename 6 complete!");
+  });
+
+  fs.rename("fixedPredictions6tomorrow", "fixedPredictions6today.json", (err) => {
+    if (err) throw err;
+    console.log("Rename 7 complete!");
+  });
+
+  fs.rename("fixedPredictions10tomorrow", "fixedPredictions10today.json", (err) => {
+    if (err) throw err;
+    console.log("Rename 8 complete!");
+  });
+
 });
