7df06c32c762c6fe744fbd338a24dbe8b2527d7c
Greg Dorward <greg.dorward@red-badger.com>
2021-02-11 17:14:51 +0000

    another fix

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ const { response } = require("express");
 // const port = process.env.PORT || 5000;
 
 
-console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-
-
 const apiKey = process.env.API_KEY
 
 
